68d7a1ad436e42db6cd5643fb2850d83c52cd35c
Valentina Corak <valentinacorak@gmail.com>
2021-07-15 16:53:47 +0200

    Testing...

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 
-const PORT = 4500;
+const PORT = 4500 ;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}) );
