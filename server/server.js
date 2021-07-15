121c21d7357afefeac68517ab15cbbb652e662ce
Valentina Corak <valentinacorak@gmail.com>
2021-07-15 15:01:15 +0200

    Added ngModule to every input- data binding

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 
-const PORT = 9012;
+const PORT = 4500;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}) );
