2fb2db557677da3ff4eee090636f4f30f53c86dd
Aaron Den Hond <aaron.denhond@gmail.com>
2021-07-15 12:26:14 +0200

    added dropdown to animal classes

--- animalDB/src/app/app.component.ts
+++ animalDB/src/app/app.component.ts
@@ -3,8 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'animalDB';
+  title = 'Zoo of Losers';
+  animalClasses: any = ['Mammal', 'Amphibian', 'Bird', 'Reptiles', 'Insects'];
 }
