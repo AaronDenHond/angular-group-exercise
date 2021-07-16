1d8216d02dc11ba53f094177d31008ae2a883d2b
Aaron Den Hond <aaron.denhond@gmail.com>
2021-07-16 15:02:57 +0200

    working flip animation!

--- animalDB/src/app/app.component.ts
+++ animalDB/src/app/app.component.ts
@@ -1,45 +1,73 @@
 import { Component, OnInit } from '@angular/core';
 import { Animal } from './animal';
-import { AddAnimalService } from "./add-animal.service";
+import { AddAnimalService } from './add-animal.service';
+import {
+  trigger,
+  state,
+  style,
+  transition,
+  animate,
+} from '@angular/animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
+  animations: [
+    trigger('flipState', [
+      state(
+        'active',
+        style({
+          transform: 'rotateY(179deg)',
+        })
+      ),
+      state(
+        'inactive',
+        style({
+          transform: 'rotateY(0)',
+        })
+      ),
+      transition('active => inactive', animate('500ms ease-out')),
+      transition('inactive => active', animate('500ms ease-in')),
+    ]),
+  ],
 })
 export class AppComponent implements OnInit {
-
-  allAnimals : any;
+  allAnimals: any;
   title = 'Zoo of Losers';
   animalClasses: any = ['Mammal', 'Amphibian', 'Bird', 'Reptiles', 'Insects'];
 
   animal = new Animal('', '', '', '', '', '');
 
-  constructor(private addAnimalService : AddAnimalService) {
-  }
+  constructor(private addAnimalService: AddAnimalService) {}
 
-  ngOnInit() : any {
-    this.getAllAnimals('http://localhost:9100/allAnimals') // have to call the function with url/allAnimals
+  ngOnInit(): any {
+    this.getAllAnimals('http://localhost:9100/allAnimals'); // have to call the function with url/allAnimals
     // console.log(this.allAnimals);
   }
+  flip: string = 'inactive';
 
+  toggleFlip() {
+    this.flip = this.flip == 'inactive' ? 'active' : 'inactive';
+  }
 
   onSubmit() {
     this.addAnimalService.addAnimal(this.animal).subscribe(
-      data => console.log('It worked', data),
-      error => console.log('It did not work', error)
-    )
+      (data) => console.log('It worked', data),
+      (error) => console.log('It did not work', error)
+    );
 
     this.getAllAnimals('http://localhost:9100/allAnimals');
   }
 
-  public async getAllAnimals (url : string) : Promise <any> {
+  public async getAllAnimals(url: string): Promise<any> {
     return await fetch(url, {
       method: 'GET',
-      headers: {'Content-Type': 'application/json'}
-    }).then(response => {
-      return response.json();
-    }).then(data => (this.allAnimals = data));
-
+      headers: { 'Content-Type': 'application/json' },
+    })
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => (this.allAnimals = data));
   }
 }
--- animalDB/src/app/app.module.ts
+++ animalDB/src/app/app.module.ts
@@ -3,10 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, FormsModule, ReactiveFormsModule, HttpClientModule],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
